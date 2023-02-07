ca749714a8ee1c8cf5b23b6eda9a3ed1a769e213
sally-ksh <sally.devz@gmail.com>
2023-02-07 23:58:25 +0900

    Add #1 AuthenticationConfiguration - permit url

---
+++ src/main/java/com/sally/sns/configuration/AuthenticationConfiguration.java
@@ -0,0 +1,24 @@
+package com.sally.sns.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class AuthenticationConfiguration {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/api/*/users/join").permitAll()
+			.antMatchers("/api/**").authenticated()
+			.and()
+			.build();
+	}
+}
