f9fa1d764f370280ad9f5565aa4f8218942c531a
sally-ksh <sally.devz@gmail.com>
2023-02-08 02:23:04 +0900

    Add #3 BaseEntity - EntityListeners

---
+++ src/main/java/com/sally/sns/model/entity/BaseEntity.java
@@ -0,0 +1,44 @@
+package com.sally.sns.model.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime modifiedAt;
+
+	@ColumnDefault("0")
+	private boolean deleted;
+
+	void toDelete() {
+		this.deleted = true;
+	}
+}
