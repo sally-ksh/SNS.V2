9ca2e985848b7ad2403b1fccedefca77228fa6bc
sally-ksh <sally.devz@gmail.com>
2023-02-08 02:22:27 +0900

    Add #1 UserEntity

---
+++ src/main/java/com/sally/sns/model/entity/UserEntity.java
@@ -0,0 +1,71 @@
+package com.sally.sns.model.entity;
+
+import static lombok.AccessLevel.PRIVATE;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.sally.sns.model.UserRole;
+
+import org.hibernate.annotations.Where;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = true)
+@Where(clause = "deleted = false")
+@Table(name = "user_tb")
+@Entity
+public class UserEntity extends BaseEntity {
+	private String nickName;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private UserRole role = UserRole.USER;
+
+	@Builder(access = PRIVATE)
+	public UserEntity(
+		Integer id,
+		LocalDateTime createdAt,
+		LocalDateTime modifiedAt,
+		boolean deleted,
+		String nickName,
+		String password,
+		UserRole role) {
+		super(id, createdAt, modifiedAt, deleted);
+		this.nickName = nickName;
+		this.password = password;
+		this.role = role;
+	}
+
+	public static UserEntity of(String nickName, String password) {
+		return UserEntity.builder()
+			.nickName(nickName)
+			.password(password)
+			.role(UserRole.USER)
+			.build();
+	}
+
+	public boolean isMatchUpPassword(BCryptPasswordEncoder passwordEncoder, String password) {
+		return passwordEncoder.matches(password, this.password);
+	}
+
+	public String nickName() {
+		return this.nickName;
+	}
+
+	public String roleText() {
+		return this.role.name();
+	}
+
+	public boolean hasNickName(String nickName) {
+		return this.nickName.equals(nickName);
+	}
+}
