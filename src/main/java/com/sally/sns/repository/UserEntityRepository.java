2f5ef05daf2dbd376d154c18e929e28f0de9253b
sally-ksh <sally.devz@gmail.com>
2023-02-08 17:31:56 +0900

    Add #4 UserEntityRepository - findByNickName

--- src/main/java/com/sally/sns/repository/UserEntityRepository.java
+++ src/main/java/com/sally/sns/repository/UserEntityRepository.java
@@ -5,6 +5,10 @@ import com.sally.sns.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
 	boolean existsUserByNickNameOrEmail(@Param("nickName") String nickName, @Param("email") String email);
+
+	Optional<UserEntity> findByNickName(@Param("nickName") String nickName);
 }
