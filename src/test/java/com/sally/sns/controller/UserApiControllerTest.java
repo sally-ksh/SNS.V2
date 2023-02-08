2afea22c1bf931e68bb2501b8df1fe682ab356cf
sally-ksh <sally.devz@gmail.com>
2023-02-08 17:28:30 +0900

    Test & Refactor #4 UserApiControllerTest

--- src/test/java/com/sally/sns/controller/UserApiControllerTest.java
+++ src/test/java/com/sally/sns/controller/UserApiControllerTest.java
@@ -1,9 +1,9 @@
 package com.sally.sns.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,6 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sally.sns.controller.reuqest.UserRequest;
 import com.sally.sns.exception.ErrorCode;
 import com.sally.sns.exception.SnsApplicationException;
+import com.sally.sns.model.User;
 import com.sally.sns.service.UserService;
 
 import org.junit.jupiter.api.DisplayName;
@@ -47,12 +48,12 @@ class UserApiControllerTest {
 	@WithAnonymousUser
 	@DisplayName("회원가입 요청 정상처리되어 OK 응답한다.")
 	void join_valid_ok() throws Exception {
-		doNothing().when(userService).create(any(UserRequest.Join.class));
+		User mock = mock(User.class);
+		when(userService.create(getJoinRequest())).thenReturn(mock);
 
 		mockMvc.perform(post("/api/v2/users/join")
-				.with(csrf())
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(contentMapper(nickName, password))
+				.content(contentMapper(getJoinRequest()))
 			).andDo(print())
 			.andExpect(status().isOk());
 	}
@@ -65,13 +66,63 @@ class UserApiControllerTest {
 
 		mockMvc.perform(post("/api/v2/users/join")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(contentMapper(nickName, password))
+				.content(contentMapper(getJoinRequest()))
 			).andDo(print())
 			.andExpect(status().isConflict());
 		// .andExpect(status().is4xxClientError());
 	}
 
-	private String contentMapper(String nickName, String password) throws JsonProcessingException {
-		return objectMapper.writeValueAsString(new UserRequest.Join(nickName, email, password));
+	@Test
+	@WithAnonymousUser
+	@DisplayName("로그인이 정상 동작한다.")
+	void login_valid_OK() throws Exception {
+		when(userService.login(getLoginRequest(password))).thenReturn("encoded-token");
+
+		mockMvc.perform(post("/api/v2/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(contentMapper(getLoginRequest(password)))
+			).andDo(print())
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithAnonymousUser
+	@DisplayName("로그인시 가입 안 된 닉네임은 에러 처리한다.")
+	void login_noneOfNickname_error() throws Exception {
+		when(userService.login(new UserRequest.Login(nickName, password)))
+			.thenThrow(new SnsApplicationException(ErrorCode.USER_NOT_FOUNDED, ""));
+
+		mockMvc.perform(post("/api/v2/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(contentMapper(getLoginRequest(password)))
+			).andDo(print())
+			.andExpect(status().isNotFound());
+	}
+
+	@Test
+	@WithAnonymousUser
+	@DisplayName("로그인시 요청 비밀번호가 다르면 에러 처리한다.")
+	void login_differentPassword_error() throws Exception {
+		String wrongPassword = "wrongPwd1234";
+		when(userService.login(getLoginRequest(password)))
+			.thenThrow(new SnsApplicationException(ErrorCode.INVALID_PASSWORD, ""));
+
+		mockMvc.perform(post("/api/v2/users/login")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(contentMapper(getLoginRequest(wrongPassword)))
+			).andDo(print())
+			.andExpect(status().isNotFound());
+	}
+
+	private String contentMapper(Object request) throws JsonProcessingException {
+		return objectMapper.writeValueAsString(request);
+	}
+
+	private UserRequest.Join getJoinRequest() {
+		return new UserRequest.Join(email, nickName, password);
+	}
+
+	private UserRequest.Login getLoginRequest(String password) {
+		return new UserRequest.Login(nickName, password);
 	}
 }
