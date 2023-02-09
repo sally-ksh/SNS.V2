d745768173787819b72ed7a5ad7ddeea461b37ae
sally-ksh <sally.devz@gmail.com>
2023-02-09 12:29:51 +0900

    Add #5 UserCacheRepository

---
+++ src/main/java/com/sally/sns/repository/UserCacheRepository.java
@@ -0,0 +1,41 @@
+package com.sally.sns.repository;
+
+import com.sally.sns.model.User;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Repository
+public class UserCacheRepository {
+	public static final String PREFIX_USER = "USER-";
+	private final static Duration USER_CACHE_TTL = Duration.ofDays(3);
+	private final RedisTemplate<String, User> userRedisTemplate;  // 싱글 스레드
+
+	public void save(User user) {
+		String key = getKey(user.getNickName());
+		log.info("store a user in a cache [{}:{}]", key, user);
+		userRedisTemplate.opsForValue().set(key, user, USER_CACHE_TTL);
+	}
+
+	public Optional<User> user(String nickname) {
+		String key = getKey(nickname);
+		User user = userRedisTemplate.opsForValue().get(key);
+		log.info("read a user in the cache [{}:{}]", key, user);
+		return Optional.ofNullable(user);
+	}
+
+	private String getKey(String nickname) {
+		StringBuilder buffer = new StringBuilder();
+		return buffer.append(PREFIX_USER)
+			.append(nickname)
+			.toString();
+	}
+}
