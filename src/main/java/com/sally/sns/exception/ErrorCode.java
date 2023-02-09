3cb84dda8f7f09665c21275bdd079b6167e72fb3
sally-ksh <sally.devz@gmail.com>
2023-02-09 12:30:54 +0900

    Add #5 ErrorCode - INVALID_TOKEN

--- src/main/java/com/sally/sns/exception/ErrorCode.java
+++ src/main/java/com/sally/sns/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Sorry, we need a time."),
 	DUPLICATED_USER_NAME(HttpStatus.CONFLICT, "User name is duplicated."),
 	USER_NOT_FOUNDED(HttpStatus.NOT_FOUND, "No information of user."),
-	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "The password is wrong.");
+	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "The password is wrong."),
+	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "The token is invalid.");
 
 	private HttpStatus httpStatus;
 	private String message;
