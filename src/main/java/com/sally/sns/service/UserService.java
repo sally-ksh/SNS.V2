d9ec74644b6ce74697a484a0c1a6782a5362b935
sally-ksh <sally.devz@gmail.com>
2023-02-09 12:30:34 +0900

    Add #5 UserService - cache

--- src/main/java/com/sally/sns/service/UserService.java
+++ src/main/java/com/sally/sns/service/UserService.java
@@ -6,12 +6,15 @@ import com.sally.sns.exception.ErrorCode;
 import com.sally.sns.exception.SnsApplicationException;
 import com.sally.sns.model.User;
 import com.sally.sns.model.entity.UserEntity;
+import com.sally.sns.repository.UserCacheRepository;
 import com.sally.sns.repository.UserEntityRepository;
 import com.sally.sns.util.JwtTokenUtils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +24,7 @@ public class UserService {
 	private final UserEntityRepository userEntityRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
 	private final JwtConfiguration jwtConfiguration;
+	private final UserCacheRepository userCacheRepository;
 
 	@Transactional
 	public User create(UserRequest.Join request) {
@@ -36,16 +40,21 @@ public class UserService {
 
 	@Transactional(readOnly = true)
 	public String login(UserRequest.Login userLoginRequest) {
+		if (getUserFromCache(userLoginRequest.getNickname()).isPresent()) {
+			return toJwtToken(userLoginRequest);
+		}
 		UserEntity userEntity = getUserEntityOrThrow(userLoginRequest.getNickname());
 		if (!userEntity.isMatchUpPassword(passwordEncoder, userLoginRequest.getPassword())) {
 			throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
 		}
+		userCacheRepository.save(User.fromEntity(userEntity));
 		return toJwtToken(userLoginRequest);
 	}
 
 	@Transactional(readOnly = true)
 	public User loadUserByUserName(String nickName) {
-		return User.fromEntity(getUserEntityOrThrow(nickName));
+		return getUserFromCache(nickName)
+			.orElse(User.fromEntity(getUserEntityOrThrow(nickName)));
 	}
 
 	private void isUniqueNickname(UserRequest.Join userJoinRequest) {
@@ -68,5 +77,9 @@ public class UserService {
 			userLoginRequest.getNickname(),
 			jwtConfiguration.getSecretKey(),
 			jwtConfiguration.getExpiredTimeMs());
+	}
+
+	private Optional<User> getUserFromCache(String nickname) {
+		return userCacheRepository.user(nickname);
 	}
 }
