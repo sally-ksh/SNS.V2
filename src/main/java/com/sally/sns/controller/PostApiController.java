61a2f101d97985f28222498437c970bed90449ce
sally-ksh <sally.devz@gmail.com>
2023-02-14 21:32:10 +0900

    Add #7 PostApiController

--- src/main/java/com/sally/sns/controller/PostApiController.java
+++ src/main/java/com/sally/sns/controller/PostApiController.java
@@ -1,10 +1,14 @@
 package com.sally.sns.controller;
 
+import com.sally.sns.controller.response.PostResponse;
 import com.sally.sns.controller.response.Response;
 import com.sally.sns.controller.reuqest.PostRequest;
 import com.sally.sns.service.PostService;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,10 @@ public class PostApiController {
 	public Response write(@RequestBody PostRequest.Creation request, Authentication authentication) {
 		postService.create(request, authentication.getName());
 		return Response.success();
+	}
+
+	@GetMapping
+	public Response<Page<PostResponse>> getLists(Pageable pageable, Authentication authentication) {
+		return Response.success(postService.readAll(pageable).map(PostResponse::of));
 	}
 }
