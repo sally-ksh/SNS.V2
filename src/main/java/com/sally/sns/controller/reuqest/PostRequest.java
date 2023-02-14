e68483c08f0754c5b453120ab85a11732c8f14a9
sally-ksh <sally.devz@gmail.com>
2023-02-14 21:04:59 +0900

    Add #7 Creation - PlaceInfo

---
+++ src/main/java/com/sally/sns/controller/reuqest/PostRequest.java
@@ -0,0 +1,40 @@
+package com.sally.sns.controller.reuqest;
+
+import com.sally.sns.model.Location;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class PostRequest {
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Creation {
+		private String title;
+		private String content;
+		private PlaceInfo place;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class PlaceInfo {
+		private String name;
+		private double x;
+		private double y;
+
+		public Location toLocation() {
+
+			return Location.of(name, toLongitude(), toLatitude());
+		}
+
+		public double toLongitude() {
+			return this.x;
+		}
+
+		public double toLatitude() {
+			return this.y;
+		}
+	}
+}
