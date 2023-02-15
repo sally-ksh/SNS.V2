4e3c2c303e40f78545dd9894d7983db558e3d35b
sally-ksh <sally.devz@gmail.com>
2023-02-15 16:31:04 +0900

    Test #7 PostServiceTest - modify

--- src/test/java/com/sally/sns/service/PostServiceTest.java
+++ src/test/java/com/sally/sns/service/PostServiceTest.java
@@ -2,6 +2,7 @@ package com.sally.sns.service;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -12,6 +13,7 @@ import com.sally.sns.exception.SnsApplicationException;
 import com.sally.sns.fixture.entity.TestPostEntity;
 import com.sally.sns.fixture.request.PostRequestFactory;
 import com.sally.sns.model.User;
+import com.sally.sns.model.entity.PostEntity;
 import com.sally.sns.repository.PostEntityRepository;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +25,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
@@ -66,7 +70,7 @@ class PostServiceTest {
 
 	@Test
 	@DisplayName("포스트 목록 조회가 정상 동작 한다.")
-	void post_readAll_ok() {
+	void readAll_valid_ok() {
 		Pageable pageable = mock(Pageable.class);
 		when(postEntityRepository.findPostViews(pageable))
 			.thenReturn(Page.empty());  // Collections.emptyList()
@@ -78,7 +82,7 @@ class PostServiceTest {
 
 	@Test
 	@DisplayName("나의 포스트 목록 조회가 정상 동작 한다.")
-	void post_readMemberPosts_ok() {
+	void readMemberPosts_valid_ok() {
 		Pageable pageable = mock(Pageable.class);
 		when(postEntityRepository.findAllByAuthorId(testPostEntity.authorId(), pageable))
 			.thenReturn(Page.empty());
@@ -88,7 +92,49 @@ class PostServiceTest {
 		assertThatNoException();
 	}
 
+	@Test
+	@DisplayName("포스트 수정기능은 정상 동작한다")
+	void modify_valid_ok() {
+		when(postEntityRepository.findWithAuthorById(any()))
+			.thenReturn(Optional.of(mock(PostEntity.class)));
+
+		postService.modify(getModificationRequest(), testPostEntity.postId(), testPostEntity.authorId());
+
+		assertThatNoException();
+	}
+
+	@Test
+	@DisplayName("포스트 수정시 해당 포스트가 없는 경우 에러 발생한다")
+	void modify_noExistPost_error() {
+		when(postEntityRepository.findWithAuthorById(any()))
+			.thenReturn(Optional.empty());
+
+		assertThatThrownBy(
+			() -> postService.modify(getModificationRequest(), testPostEntity.postId(), testPostEntity.authorId()))
+			.isInstanceOf(SnsApplicationException.class)
+			.hasMessageContaining(ErrorCode.POST_NOT_FOUND.name());
+	}
+
+	@Test
+	@DisplayName("포스트 수정시 포스트의 작성자가 아닌 경우 에러 발생한다")
+	void modify_differentRequesterOfAuthor_error() {
+		PostEntity postEntity = mock(PostEntity.class);
+		Long expectedNotWriter = testPostEntity.authorId() + 1L;
+		when(postEntityRepository.findWithAuthorById(testPostEntity.postId()))
+			.thenReturn(Optional.of(postEntity));
+		when(postEntity.isNotAuthor(expectedNotWriter)).thenReturn(true);
+
+		assertThatThrownBy(
+			() -> postService.modify(getModificationRequest(), testPostEntity.postId(), expectedNotWriter))
+			.isInstanceOf(SnsApplicationException.class)
+			.hasMessageContaining(ErrorCode.INVALID_AUTHORIZATION.name());
+	}
+
 	private PostRequest.Creation getPostCreationRequest(PostRequestFactory.Designation designation) {
 		return PostRequestFactory.getPostCreationRequest(TEST_TITLE, TEST_CONTENT, designation);
+	}
+
+	private PostRequest.Modification getModificationRequest() {
+		return new PostRequest.Modification(TEST_TITLE + "수정", TEST_CONTENT + "수정");
 	}
 }
