b9f72d5e53d33a1cc2bc3476e3fea3fd25868c94
sally-ksh <sally.devz@gmail.com>
2023-02-15 15:35:47 +0900

    Refactor #7 Post - Member

--- src/main/java/com/sally/sns/model/Post.java
+++ src/main/java/com/sally/sns/model/Post.java
@@ -13,6 +13,18 @@ public class Post {
 	private Long postId;
 	private String title;
 	private String content;
-	private User user;
+	private Member author;
 	private LocalDateTime createdAt;
+	private Location location;
+
+	public static Post from(PostView postView) {
+		return Post.builder()
+			.postId(postView.getPostId())
+			.title(postView.getTitle())
+			.content(postView.getContent())
+			.author(postView.getAuthor())
+			.createdAt(postView.getCreatedAt())
+			.location(postView.getLocation())
+			.build();
+	}
 }
