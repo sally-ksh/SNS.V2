81c4aa36038981f3bba221a38f101f11da587fb9
sally-ksh <sally.devz@gmail.com>
2023-02-15 16:34:33 +0900

    Add #7 Member - Post Dto

---
+++ src/main/java/com/sally/sns/model/Member.java
@@ -0,0 +1,15 @@
+package com.sally.sns.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Member {
+	private Long userId;
+	private String nickName;
+}
