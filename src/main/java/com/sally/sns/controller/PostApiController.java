4b070b0e6beafa0e375de6be08b631558c5e74d1
sally-ksh <sally.devz@gmail.com>
2023-02-17 21:17:10 +0900

    Add #7 PostApiController - delete

--- src/main/java/com/sally/sns/controller/PostApiController.java
+++ src/main/java/com/sally/sns/controller/PostApiController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,12 @@ public class PostApiController {
 		@RequestBody PostRequest.Modification request, Authentication authentication) {
 		User user = (User)authentication.getPrincipal();
 		return Response.success(MyPostResponse.of(postService.modify(request, postId, user.getId())));
+	}
+
+	@DeleteMapping("/{postId}")
+	public Response delete(@PathVariable Long postId, Authentication authentication) {
+		User user = (User)authentication.getPrincipal();
+		postService.deleteSoftly(postId, user.getId());
+		return Response.success();
 	}
 }
