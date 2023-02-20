ccc22341ff8b1d4cb8ff64366489154306b99a4d
sally-ksh <sally.devz@gmail.com>
2023-02-20 23:50:06 +0900

    Add #8 CommentRequest - Modification

--- src/main/java/com/sally/sns/controller/reuqest/CommentRequest.java
+++ src/main/java/com/sally/sns/controller/reuqest/CommentRequest.java
@@ -5,11 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 public class CommentRequest {
-
 	@Getter
 	@AllArgsConstructor
 	@NoArgsConstructor
 	public static class Creation {
+		private String body;
+	}
+
+	@Getter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class Modification {
 		private String body;
 	}
 }
