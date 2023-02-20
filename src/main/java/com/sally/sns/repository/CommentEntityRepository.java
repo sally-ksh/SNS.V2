6190799189a71b744e06ace1f022173b8da9e9d5
sally-ksh <sally.devz@gmail.com>
2023-02-20 23:50:22 +0900

    Add #8 CommentEntityRepository - findCommentViewsBy

--- src/main/java/com/sally/sns/repository/CommentEntityRepository.java
+++ src/main/java/com/sally/sns/repository/CommentEntityRepository.java
@@ -1,8 +1,21 @@
 package com.sally.sns.repository;
 
+import com.sally.sns.model.CommentView;
 import com.sally.sns.model.entity.CommentEntity;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentEntityRepository extends JpaRepository<CommentEntity, Long> {
+	@Query(value =
+		"SELECT new com.sally.sns.model.CommentView("
+			+ " c.id, c.comment, c.createdAt, u.id, u.nickName "
+			+ ") "
+			+ "FROM comment c inner join UserEntity u on c.member.id = u.id "
+			+ "where c.post.id = :postId and c.deleted = false "
+			+ "order by c.createdAt desc ")
+	Page<CommentView> findCommentViewsBy(@Param("postId") Long postId, Pageable pageable);
 }
