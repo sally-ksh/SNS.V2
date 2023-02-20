b5922ca1cee6e436691ab551f810814ffa75187c
sally-ksh <sally.devz@gmail.com>
2023-02-20 23:50:44 +0900

    Add #8 CommentEntity - update, deleteSoftly

--- src/main/java/com/sally/sns/model/entity/CommentEntity.java
+++ src/main/java/com/sally/sns/model/entity/CommentEntity.java
@@ -4,7 +4,11 @@ import static javax.persistence.FetchType.LAZY;
 import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
+import com.sally.sns.exception.ErrorCode;
+import com.sally.sns.exception.SnsApplicationException;
+
 import org.hibernate.annotations.Where;
+import org.springframework.util.StringUtils;
 
 import java.time.LocalDateTime;
 
@@ -49,12 +53,35 @@ public class CommentEntity extends BaseEntity {
 		this.member = member;
 		this.post = post;
 	}
-	
+
 	public static CommentEntity of(String comment, UserEntity userEntity, PostEntity postEntity) {
 		return CommentEntity.builder()
 			.comment(comment)
 			.member(userEntity)
 			.post(postEntity)
 			.build();
+	}
+
+	public boolean hasNotPost(Long postId) {
+		return !this.post.isSameAs(postId);
+	}
+
+	public boolean isNotCommenter(Long commenterId) {
+		return !this.member.hasId(commenterId);
+	}
+
+	public void update(String body) {
+		if (!StringUtils.hasText(body)) {
+			throw new SnsApplicationException(ErrorCode.NO_TEXT, "CommentEntity");
+		}
+		this.comment = body;
+	}
+
+	public String content() {
+		return this.comment;
+	}
+
+	public void deleteSoftly() {
+		super.toDelete();
 	}
 }
