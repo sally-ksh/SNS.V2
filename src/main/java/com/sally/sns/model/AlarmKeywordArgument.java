896464ed03c1a8456d53287dd31540b9013a9510
sally-ksh <sally.devz@gmail.com>
2023-02-27 17:12:56 +0900

    Add #9 AlarmKeywordArgument

---
+++ src/main/java/com/sally/sns/model/AlarmKeywordArgument.java
@@ -0,0 +1,23 @@
+package com.sally.sns.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlarmKeywordArgument {
+	private Long recipientId;
+	private Long senderId;
+	private Long postId;
+	private Long commentId;
+
+	public static AlarmKeywordArgument addAlarmForComment(
+		Long recipientId,
+		Long senderId,
+		Long postId,
+		Long commentId) {
+		return new AlarmKeywordArgument(recipientId, senderId, postId, commentId);
+	}
+}
