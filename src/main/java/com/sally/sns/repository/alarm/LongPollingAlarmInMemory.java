43075c0c9edfbf446701ded0a35c35d471a59f85
sally-ksh <sally.devz@gmail.com>
2023-02-27 17:14:25 +0900

    Add #9 LongPollingAlarmInMemory

---
+++ src/main/java/com/sally/sns/repository/alarm/LongPollingAlarmInMemory.java
@@ -0,0 +1,29 @@
+package com.sally.sns.repository.alarm;
+
+import com.sally.sns.model.AlarmSession;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+@Repository
+public class LongPollingAlarmInMemory {
+	private static LinkedBlockingDeque<AlarmSession> queue = new LinkedBlockingDeque(10);
+
+	public void add(AlarmSession session) {
+		System.out.println("LongPollingAlarmInMemory.add");
+		queue.addLast(session);
+	}
+
+	public AlarmSession isPeek() {
+		return queue.peekFirst();
+	}
+
+	public void remove() {
+		queue.removeIf(session -> session.isExpiredOrNoResult());
+	}
+
+	public boolean hasSession() {
+		return !queue.isEmpty();
+	}
+}
