474811493ec3547292c2f426f80d721088261a36
sally-ksh <sally.devz@gmail.com>
2023-03-06 22:51:22 +0900

    Refactor #9 sth related of Alarm

--- src/main/java/com/sally/sns/model/alarm/AlarmSession.java
+++ src/main/java/com/sally/sns/model/alarm/AlarmSession.java
@@ -1,11 +1,7 @@
 package com.sally.sns.model.alarm;
 
-import java.util.List;
-
 public interface AlarmSession {
 	Long recipientId();
-
-	void send(List<Alarm> alarms);
 
 	boolean isExpiredOrNoResult();
 
--- src/main/java/com/sally/sns/service/alarm/AlarmService.java
+++ src/main/java/com/sally/sns/service/alarm/AlarmService.java
@@ -1,11 +1,14 @@
 package com.sally.sns.service.alarm;
 
-import com.sally.sns.model.Member;
 import com.sally.sns.model.alarm.AlarmKeywordArgument;
 import com.sally.sns.model.alarm.AlarmSession;
 
-public interface AlarmService {
-	<T> T connect(AlarmSession alarmSession);
+public interface AlarmService<T> {
+	void connect(AlarmSession alarmSession);
 
-	void storeCommentAlarm(Member member, AlarmKeywordArgument keywordArgument);
+	void storeCommentAlarm(AlarmKeywordArgument keywordArgument);
+
+	default void sendAndRemove(boolean sender) {
+		
+	}
 }
