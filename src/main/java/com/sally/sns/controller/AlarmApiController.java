d1d623af0715ef9a928b87ec68680acfcf2a735e
sally-ksh <sally.devz@gmail.com>
2023-03-08 23:47:32 +0900

    Add & Refactor #10 AlarmApiController - GET /sse/subscribe

--- src/main/java/com/sally/sns/controller/AlarmApiController.java
+++ src/main/java/com/sally/sns/controller/AlarmApiController.java
@@ -1,16 +1,20 @@
 package com.sally.sns.controller;
 
 import static com.sally.sns.service.alarm.LongPollingAlarmService.LONG_POLLING_TIMEOUT;
+import static com.sally.sns.service.alarm.SseAlarmService.SSE_TIMEOUT;
 
+import com.sally.sns.controller.response.AlarmResponse;
 import com.sally.sns.controller.response.Response;
 import com.sally.sns.filter.AuthenticationUser;
 import com.sally.sns.filter.SecurityUser;
 import com.sally.sns.model.Member;
 import com.sally.sns.model.alarm.Alarm;
 import com.sally.sns.model.alarm.LongPollingSession;
+import com.sally.sns.model.alarm.SseSession;
 import com.sally.sns.service.alarm.AlarmService;
 import com.sally.sns.util.TypeCastingUtils;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,20 +44,32 @@ public class AlarmApiController {
 			final Member recipient = toMember(authentication);
 			alarmService.connect(LongPollingSession.of(recipient, output));
 		});
+		output.onTimeout(() -> {
+			log.info(AlarmResponse.TIME_OUT.name());
+			alarmService.sendAndRemove(
+				output.setErrorResult(AlarmResponse.TIME_OUT.make(String.valueOf(LONG_POLLING_TIMEOUT))));
+		});
 		return output;
 	}
 
-	@GetMapping("/sse/subscribe")
+	@GetMapping(value = "/sse/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	public SseEmitter subscribe(Authentication authentication) {
-		// final Member recipient = toMember(authentication);
-		// return alarmService.connect(SseSession.of(recipient));
+		SseEmitter sseEmitter = new SseEmitter(SSE_TIMEOUT);
+
+		final Long userId = toMember(authentication).getUserId();
+		alarmService.connect(SseSession.of(userId, sseEmitter));
+		return sseEmitter;
 	}
 
-	private Member toMember(Authentication authentication) {
-		AuthenticationUser user = TypeCastingUtils.fromAndSecTo(
+	private AuthenticationUser toAuthenticationUser(Authentication authentication) {
+		return TypeCastingUtils.fromAndSecTo(
 			authentication.getPrincipal(),
 			SecurityUser.class,
 			AuthenticationUser.class);
+	}
+
+	private Member toMember(Authentication authentication) {
+		AuthenticationUser user = toAuthenticationUser(authentication);
 		return new Member(user.getId(), user.getNickname());
 	}
 }
