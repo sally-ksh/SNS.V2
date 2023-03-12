8d88ae6ffb825b38ac98a7798ed2b9e2d1bf044d
sally-ksh <sally.devz@gmail.com>
2023-03-13 01:06:56 +0900

    Add & Refactor #11 RedisConfiguration - redisMessageListenerContainer

--- src/main/java/com/sally/sns/configuration/RedisConfiguration.java
+++ src/main/java/com/sally/sns/configuration/RedisConfiguration.java
@@ -2,6 +2,7 @@ package com.sally.sns.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sally.sns.model.User;
+import com.sally.sns.model.alarm.Alarm;
 
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -29,13 +31,37 @@ public class RedisConfiguration {
 
 	@Bean
 	public RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-		var serializer = new Jackson2JsonRedisSerializer<>(User.class);
-		serializer.setObjectMapper(objectMapper);
+		Jackson2JsonRedisSerializer<User> serializer = getSerializer(User.class);
 
 		RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(redisConnectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(serializer);
 		return redisTemplate;
+	}
+
+	@Bean
+	public RedisTemplate<String, Alarm> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		Jackson2JsonRedisSerializer<Alarm> serializer = getSerializer(Alarm.class);
+		
+		RedisTemplate<String, Alarm> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		redisTemplate.setKeySerializer(new StringRedisSerializer());  // TODO 확인해보기
+		redisTemplate.setValueSerializer(serializer);
+		return redisTemplate;
+	}
+
+	@Bean
+	RedisMessageListenerContainer redisMessageListenerContainer() {
+		final RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(redisConnectionFactory());
+		return container;
+	}
+
+	private <T> Jackson2JsonRedisSerializer<T> getSerializer(Class<T> clazz) {
+		var serializer = new Jackson2JsonRedisSerializer<>(clazz);
+		serializer.setObjectMapper(objectMapper);
+		return serializer;
 	}
 }
