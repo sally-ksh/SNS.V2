53fc0e19ad8a1a608a15344c5cb2115445b0cfc4
sally-ksh <sally.devz@gmail.com>
2023-03-14 23:48:35 +0900

    Refactor #11 AlarmRecipient - for AlarmApiController, LongPollingAlarmApiController

---
+++ src/main/java/com/sally/sns/controller/AlarmRecipient.java
@@ -0,0 +1,22 @@
+package com.sally.sns.controller;
+
+import com.sally.sns.filter.AuthenticationUser;
+import com.sally.sns.filter.SecurityUser;
+import com.sally.sns.model.Member;
+import com.sally.sns.util.TypeCastingUtils;
+
+import org.springframework.security.core.Authentication;
+
+public abstract class AlarmRecipient {
+	protected AuthenticationUser toAuthenticationUser(Authentication authentication) {
+		return TypeCastingUtils.fromAndSecTo(
+			authentication.getPrincipal(),
+			SecurityUser.class,
+			AuthenticationUser.class);
+	}
+
+	protected Member toMember(Authentication authentication) {
+		AuthenticationUser user = toAuthenticationUser(authentication);
+		return new Member(user.getId(), user.getNickname());
+	}
+}
